d4ddf63faa2a788aae36476314e3bbc476463330
Don-Smilo <104557299+Don-Smilo@users.noreply.github.com>
2022-07-27 11:35:44 +0300

    Update labyrinth.cpp

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -1,26 +1,24 @@
-﻿#include <ctime>
-#include <cstdlib>
 #include "Functions.h"
 using namespace std;
 
 
 
 int main() {
-    int cols, rows;
-    cell** matrix;
-    srand(time(0));
-    cout << "number of rows: ";
-    cin >> rows;
-    cout << "number of columns: ";
-    cin >> cols;
-    if (rows % 2 == 0)
-        rows++;
-    if (cols % 2 == 0)
-        cols++;
-    matrix = new cell* [rows];
+    int cols=51, rows=51;
+    Cell** matrix;
+    //cout << "number of rows: ";
+    //cin >> rows;
+    //cout << "number of columns: ";
+    //cin >> cols;
+    //if (rows % 2 == 0)
+    //    rows++;
+    //if (cols % 2 == 0)
+    //    cols++;
+    matrix = new Cell * [rows];
     for (int i = 0; i < cols; i++) {
-        matrix[i] = new cell[cols];
+        matrix[i] = new Cell[cols];
     }
-    MakeMatr(matrix, rows, cols);
+    MakeTemp(matrix, rows, cols);
+    GenLabyrinth(matrix, rows, cols);
     Output(matrix, rows, cols);
 }
